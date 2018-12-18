b1d4744170f83e03217d6949bf731d854a71cab3
marc <mfmunic@gmail.com>
2018-12-18 15:12:09 -0800

    mailedit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/messages", (req, res) => {
+  nodemailer.createTestAccount((err, account) => {
+    if (err) {
+      console.error("Failed to create a testing account. " + err.message);
+      return process.exit(1);
+    }
+
+    console.log("Credentials obtained, sending message...");
+    // Message object
+    let message = {
+      from: `${req.body.address}`,
+      to: "ga5pcypj6echogcn@ethereal.email",
+      subject: `${req.body.subject}`,
+      text: `${req.body.content}`,
+      html: `<ul><li>${req.body.address}</li><li>${req.body.subject}</li><li>${
+        req.body.content
+      }</li></ul>`
+    };
+
+    // Create a SMTP transporter object
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: "ga5pcypj6echogcn@ethereal.email",
+        pass: "JK991JtF8ndhcHw2hg"
+      }
+    });
+
+    transporter.sendMail(message, (err, info) => {
+      if (err) {
+        console.log("Error occurred. " + err.message);
+        return process.exit(1);
+      }
+
+      console.log("Message sent: %s", info.messageId);
+      // Preview only available when sending through an Ethereal account
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is on ${PORT}`);
+});
